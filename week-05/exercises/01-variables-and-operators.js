f3449cc54eb001e34b2dd2ee991d83f9c18bee28
gabiholl <gabrielahollmann@gmail.com>
2023-04-14 18:07:53 -0300

    BaSP-W05-2023: Variables and operators

--- week-05/exercises/01-variables-and-operators.js
+++ week-05/exercises/01-variables-and-operators.js
@@ -6,3 +6,19 @@ console.log("-Exercise 1.a");
 var a = 6;
 var b = 4;
 console.log('El resultado es',a + b);
+
+//b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+console.log("-Exercise 1.b")
+var nombre = "Anna";
+var apellido = "Rodriguez";
+var nombre_completo = nombre + " " + apellido
+console.log('Ella es ' + nombre_completo);
+
+//c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
+//guardando el resultado de la suma en una 3er variable (utilizar length).
+
+var nombre = "Silvio";
+var apellido = "Gómez";
+var cantidad = (nombre+apellido).length
+console.log('Resultado ' + cantidad);
